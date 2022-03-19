23048da99733a9f1850fdf7b82aebc91592d16a9
khaleel-mehboob <khaleel.mehboob@gmail.com>
2022-03-20 01:02:37 +0500

    Initial commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
         <h2>Hi there!</h2>
         <div>
           <a href='/auth/google'>
