593dd162babacc0ad9197763ce4941cf4800d5da
khaleel-mehboob <khaleel.mehboob@gmail.com>
2022-03-19 23:20:21 +0500

    Initital commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hi there!</h2>
-        <a href='/auth/google'>
-          Sign in with Google
-        </a>
-        <a hef='/auth/facebook'>
-          Sign in with Facebook
-        </a>
+        <div>
+          <a href='/auth/google'>
+            Sign in with Google
+          </a>
+          &nbsp;
+          <a href='/auth/facebook'>
+            Sign in with Facebook
+          </a>
+        </div>
       </header>
     </div>
   );
