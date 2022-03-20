effe94d2c0b12cfc6f88a26092fde1da64762078
khaleel-mehboob <khaleel.mehboob@gmail.com>
2022-03-20 23:53:41 +0500

    Initial Commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -50,9 +50,9 @@ const router = express.Router();
 router.get('/auth/google', passport.authenticate('google', {
   scope: ['profile', 'email']
 }));
-router.get('/auth/google/callback', passport.authenticate('google'));
+router.get('/auth/google/callback', passport.authenticate('google'), (req, res) => res.redirect('/'));
 router.get('/auth/facebook', passport.authenticate('facebook'));
-router.get('/auth/facebook/callback', passport.authenticate('facebook'));
+router.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => res.redirect('/'));
 router.get('/api/v1/auth/logout', (req, res) => {
   req.logout();
   res.redirect('/');
